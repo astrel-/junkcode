ba9a69827ee1764df430f66f094d9b5e21b2c31b
astrel- <indigoastrel@gmail.com>
2013-07-17 01:34:02 +0400

    0.079s

---
+++ euler16.py
@@ -0,0 +1,27 @@
+#Project Euler
+#Problem 16
+#real	0m0.079s
+
+#1
+def doubleNumber(li):
+    """
+    a is reversed list of digits in number
+    returns reversed list of digits in 2*number
+    """
+    digit = 0
+    for idx, val in enumerate(li):
+        if 2*val+digit>9:
+            li[idx] = 2*val-10+digit
+            digit = 1
+        else:
+            li[idx] = 2*val+digit
+            digit = 0
+    if digit: li.append(digit)
+    return li
+            
+li = [1]
+n = 1000
+for i in range(n):
+    li = doubleNumber(li)
+print sum(li)
+    
